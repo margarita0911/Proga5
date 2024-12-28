e31dcf5758e8dec00853bda3c962aca9e05e7a47
margarita0911 <mmargo0911@gmail.com>
2024-12-28 16:15:00 +0700

    Update Proga05.cpp

--- Proga05.cpp
+++ Proga05.cpp
@@ -7,9 +7,33 @@
 #include <windows.h>
 #include <vector>
 
-const int RESIDENTS_COUNT = 5;
+const int RESIDENTS_COUNT = 3;
 const char* FILENAME = "residents2.txt";
 const char* RESULT_FILENAME = "results2.txt";
+#include <string>
+
+class VisitorPass {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::string id;
+    std::string hairColor;
+    std::string dressCode;
+
+public:
+    void setVisitor(const std::string& firstName, const std::string& lastName,
+        const std::string& id, const std::string& hairColor,
+        const std::string& dressCode) {
+        this->firstName = firstName;
+        this->lastName = lastName;
+        this->id = id;
+        this->hairColor = hairColor;
+        this->dressCode = dressCode;
+    }
+
+    std::string getFirstName() const { return firstName; }
+    std::string getLastName() const { return lastName; }
+};
 
 class Appearance {
 private:
@@ -58,7 +82,7 @@ public:
     const std::string& getPhone() const { return phone; }
 
     void displayInfo() const {
-        std::cout << "Resident Info: " << firstName << " " << lastName << ", ID: " << id << ", Phone: " << phone << "\n";
+        std::cout << "Информация о человеке: " << firstName << " " << lastName << ", ID: " << id << ", телефон: " << phone << "\n";
     }
 };
 
@@ -108,5 +132,36 @@ int main() {
 
     Game game;
     game.run();
+
+    // Работа с одномерным массивом объектов
+    const int visitorCount = 2;
+    VisitorPass visitors[visitorCount]{};
+    visitors[0].setVisitor("Старушка", "Шапокляк", "12", "Седой", "официальный");
+    visitors[1].setVisitor("Крокодил", "Гена", "36", "зеленый", "формальный");
+
+    std::cout << "\nПосетители:\n";
+    for (int i = 0; i < visitorCount; ++i) {
+        std::cout << "Имя: " << visitors[i].getFirstName() << ", Фамилия: " << visitors[i].getLastName() << "\n";
+    }
+
+    // Работа с двумерным массивом объектов
+    const int groupCount = 2;
+    const int visitorsPerGroup = 2;
+    VisitorPass visitorGroups[groupCount][visitorsPerGroup]{};
+
+    visitorGroups[0][0].setVisitor("Group1_Visitor1", "G1_Last1", "01", "Brown", "Casual");
+    visitorGroups[0][1].setVisitor("Group1_Visitor2", "G1_Last2", "02", "Black", "Formal");
+    visitorGroups[1][0].setVisitor("Group2_Visitor1", "G2_Last1", "03", "Blonde", "Sporty");
+    visitorGroups[1][1].setVisitor("Group2_Visitor2", "G2_Last2", "04", "Red", "Casual");
+
+    std::cout << "\nГруппы посетителей:\n";
+    for (int i = 0; i < groupCount; ++i) {
+        std::cout << "Группа " << (i + 1) << ":\n";
+        for (int j = 0; j < visitorsPerGroup; ++j) {
+            std::cout << "Имя: " << visitorGroups[i][j].getFirstName() << ", Фамилия: " << visitorGroups[i][j].getLastName() << "\n";
+        }
+    }
+
+    return 0;
 
 }
